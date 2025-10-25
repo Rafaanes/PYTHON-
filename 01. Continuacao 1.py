fce38e07e50d1ac31fd97a0cc213b2d5dcab7635
Rafaanes <Rafaanes1@hotmail.com>
2025-10-24 22:00:55 -0300

    Add files via upload

--- 01. Continuacao 1.py
+++ 01. Continuacao 1.py
@@ -46,11 +46,151 @@ salva tudo num arquivo novo pronto para ser usado.
 
 '''
 # Importa a lib
+import pandas as pd
+import os
 
+#1o função - que recebe o caminho do arquivo e realiza o tratamento dos dados
+def tratamento(caminho):
 
+    #aba planilha 4
+    dados_p1 = pd.read_excel(
+        caminho, #caminho do arquivo excel
+        sheet_name=3, #ler a planilha de indice 3(aba)[começa a contagem do 0]
+        #pular as 4 primeiras linhas (indice leia-se: 0,1,2,3...) desta maneira localiza o cabeçalho
+        skiprows=4,
+        #dados ausentes/células vazias
+        na_values=['N/A','NaN'],
+        #ler somente as colunas 'Cliente' e 'Cidade'
+        usecols=['Cliente','Cidade']
+    )
+    #aba Planilha 2
+    dados_p2 = pd.read_excel(
+        caminho,
+        sheet_name=1,
+        skiprows=10,
+        na_values= ['N/A','NaN'],
+        usecols=['Plano Vendido','Valor Mensal'],
+        dtype= {'Valor Mensal': float} #chaves para ler valor
+    )
 
+    #arredondar os valores da coluna [Valor Mensa] em duas casas decimais
+    dados_p2['Valor Mensal'] = dados_p2['Valor Mensal'].round(2)
 
+    #remove linhas como valores ausentes em ambos os DataFrames
+    dados_p1= dados_p1.dropna() #drop(apagar na- not available/não disponivel)
+    dados_p2= dados_p2.dropna()
 
+    #juntar os dois DataFrames em 1 só, linha a linha pelo indice
+    dados = pd.merge(dados_p1,dados_p2,
+                     left_index=True, #mesclar usando o indice dos dados_p1(esquerda)
+                     right_index=True #mesclar usando o indice dos dados_p2(direita)
+                     )
+    
+    #comentario que vem a seguir pode ser feito fora da função, mas está aqui devido a estrutura dos dados
+    #reset (limpar) o indice do DataFrame para começar do zero, descartando o indice anterior
+    #drop=True(apagar o indice) inplace=True(no proprio DataFrame)
+    dados.reset_index(drop=True,inplace=True)
+
+    #ajustar o indice para começar em  1 ao inves de o(mais amigavel)
+    #tradução: dados.index = dados,index +1
+    dados.index += 1
+
+    #guardar o indice atual (que vai servir como ID)
+    id = dados.index
+
+    #adicionar uma coluna [ID] numerada sequencialmente
+    dados.insert(
+        loc=0, #insete na posição 0 (primeira coluna)
+        column='ID', #nome da coluna
+        value=id #valores para essa coluna (indice)
+    )
+
+    #adicionar uma coluna chamada [País] como valor fixo [Brasil] para todas as linhas
+    dados.insert(
+        loc=3, #inserir na posição 3(quarta coluna, começa com zero)
+        column='País', #coluna nova
+        value='Brasil' #valor fixo para todas as linhas 
+    )
+
+    #### ATENÇÃO ####
+    #vamos criar uma função: desconto_por_plano
+    #cria uma nova coluna chamada [Desconto] na tabela = [dados]
+    #para cada linha da tabela, aplica a função desconto_por_plano
+    dados['Desconto'] = dados.apply(
+        #usa a função anõnima(lambda) que recebe cada linha(row) e chama a função desconto_por_plano, passando os valores da linha
+        lambda row: desconto_por_plano(
+            row['Plano Vendido'],
+            row['Valor Mensal']),
+            # axis = 1 (calcular linha a linha em vez de coluna)
+        axis=1
+            
+    )
+    #retorna os dados tratados e caminho do arquivo 
+    return dados, caminho 
+
+#2a função - localiza no DataFrame o tipo de plano e aplica o desconto
+def desconto_por_plano(plano,valor_mensal):
+    #função que calcula o valor com desconto, conforme o tipo de plano
+    if plano == 'Pro':
+        return valor_mensal * (1 - 0.05) #desconto de 5%
+    elif plano == 'Regular':
+        return valor_mensal * (1 - 0.10) #desconto de 10%
+    else:
+        return valor_mensal * (1 - 0.15) # desconto de 15%
+    
+#3a função
+#index=False (informa ao pandas, que você não quer salvar o indice do DataFrame no Excel como primeira coluna)7
+def salvar_novoarquivo(dados, nome_planilha,caminho_novo):
+    try:
+        dados.to_excel(caminho_novo, sheet_name=nome_planilha, index=False)
+        print(f'Arquivo salvo no local: {caminho_novo}')
+        os.startfile(caminho_novo)
+    except:
+        print('ATENÇÃO: o arquivo está aberto... feche e tente novamente')
+###########################3 INICIO DO PROGRAMA ########################################
+#1o passo - identificações dos arquivos
+caminho_origem = r'C:\Rafaélla Sena - Python\Python 2\Python 2 - Clarify\Aluno\Aula_01\bases\01_base_exe.xlsx'
+caminho_novo_arquivo = r'C:\Rafaélla Sena - Python\Python 2\Python 2 - Clarify\Aluno\Aula_01\bases\Resultado_base_exe.xlsx'
+
+#nome da aba para o novo arquivo
+nome_planilha = 'Relatório de vendas'
+
+#2o passo - nesta estapa inicia o proceso de análise, onde vamos criar as funções: Salvar_novoarquivo e tratamento para executar o tratamento dos dados e salvar em novo arquivo.xlsx
+salvar_novoarquivo(
+    #pegar apenas o DataFrame da tupla retornada
+    tratamento(caminho_origem)[0],
+    nome_planilha,
+    caminho_novo_arquivo
+)
+
+#3o passo
+caminho = caminho_novo_arquivo
+
+#lê o arquivo Excel tratado
+df = pd.read_excel(caminho)
+
+#reset do indice para começar do zero
+df.reset_index(drop=True, inplace=True)
+
+#ajustar o indice
+df.index += 1
+
+#criar varialvel id baseada no indice
+id = df.index
+
+#se a coluna [ID] existir nós vamos atualizar, senão encerre
+if 'ID' in df.columns:
+    df['ID'] = id
+else:
+    df.insert(loc=0, column = 'ID', value = 0)
+
+#se a coluna [País] existir, atualiza, senão encerre
+
+if 'País' in df.columns:
+    df ['País'] = 'Brasil'
+else:
+    df.insert(loc=3, column='País', value='Brasil')
+print(df)
 
 
 
--- 01. Continuacao 2.py
+++ 01. Continuacao 2.py
@@ -1,8 +1,33 @@
 # importar openpyxl
+#lib que permite realizar formatações no excel
+from openpyxl import load_workbook
+import os
 
+#item - caminho do Excel
+arquivo_excel = r'C:\Rafaélla Sena - Python\Python 2\Python 2 - Clarify\Aluno\Aula_01\bases\Resultado_base_exe.xlsx'
 
+#item 2 - carregar o arquivo Excel existente
+# wb = Work Book
+wb = load_workbook(arquivo_excel)
 
+#item 3 - selecionar uma aba
+#ws = Work Sheet
+ws = wb['Relatório de vendas']
 
+#item 4 - aplica a formatação de número(decimal com 2 casas) nas colunas F e G a partir da linha 2
+#tradução: para cada linha dentro da planilha, inicie da linha 2. ws.max_row captura a quantidade de linhas preenchidas, ao somar mais 1, seu objetivo é descobrir a próxima linha vazia para saber o momento de parar/stop o for.
+for row in range(2, ws.max_row + 1):
+    ws[f'F{row}'].number_format = '#,##0.00'
+    ws[f'G{row}'].number_format = '#,##0.00'
+    
+try:
+#item 5 - salvar as alterações no mesmo arquivo(sobreescrever)
+    wb.save(arquivo_excel)
+    os.startfile(arquivo_excel)
+
+except: 
+    print('Atenção! Arquivo aberto, ou verifique o erro')
+    exit()
 
 
 
